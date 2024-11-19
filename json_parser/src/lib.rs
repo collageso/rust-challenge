94b95026129ab50ba929fbb48f640d859270f118
collageso <dnjsqhwo@gmail.com>
2024-11-19 23:48:27 +0900

    update code

--- json_parser/src/lib.rs
+++ json_parser/src/lib.rs
@@ -1,14 +1,13 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+pub enum Token {
+    BraceOpen,
+    BraceClose,
+    BracketOpen,
+    BracketClose,
+    Comma,
+    Colon,
+    Null,
+    True,
+    False,
+    Number(f64),
+    String(String),
 }
